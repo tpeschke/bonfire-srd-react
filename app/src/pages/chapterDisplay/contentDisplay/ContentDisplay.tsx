9377468cfb21b746bb9a55e4bfa599a2603cb2a4
Trent <mr.peschke@gmail.com>
2025-08-12 19:36:00 -0600

    adding ids to headings in markdowns

--- app/src/pages/chapterDisplay/contentDisplay/ContentDisplay.tsx
+++ app/src/pages/chapterDisplay/contentDisplay/ContentDisplay.tsx
@@ -2,6 +2,9 @@ import './ContentDisplay.css'
 import { MarkdownContent, ComponentContent } from "@srd/common/interfaces/ChapterInterfaces";
 import Markdown from "react-markdown";
 import InlineDisplay from "./InlineDisplay";
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 interface Props {
     contents: (MarkdownContent | ComponentContent)[]
@@ -11,8 +14,9 @@ export default function ContentDisplay({ contents }: Props) {
     return (
         <div className="content-display-shell">
             {contents.reduce((displayedContent: any[], content: MarkdownContent | ComponentContent) => {
+
                 if (content.type === 'markdown') {
-                    displayedContent.push(<Markdown key={displayedContent.length}>{content.body}</Markdown>)
+                    displayedContent.push(<Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings]} key={displayedContent.length}>{content.body}</Markdown>)
                 } else if (content.type === 'component') {
                     displayedContent.push(<InlineDisplay key={displayedContent.length} componentInfo={content} />)
                 }
--- backend/server/controllers/rulesGuide/rulesGuideController.ts
+++ backend/server/controllers/rulesGuide/rulesGuideController.ts
@@ -15,7 +15,78 @@ export async function getChapter(request: ChapterRequest, response: Response) {
         {
             type: 'markdown',
             body: `# Chapter ${chapter} in the ${book}
-            The beginning`
+
+react-markdown is a markdown component for React.
+
+👉 Changes are re-rendered as you type.
+
+👈 Try writing some markdown on the left.
+
+## Overview
+
+* Follows [CommonMark](https://commonmark.org)
+* Optionally follows [GitHub Flavored Markdown](https://github.github.com/gfm/)
+// * Renders actual React elements instead of using dangerouslySetInnerHTML
+// * Lets you define your own components (to render MyHeading instead of h1)
+* Has a lot of plugins
+
+## Table of contents
+
+## Syntax highlighting
+
+Pretty neat, eh?
+
+## GitHub flavored markdown (GFM)
+
+For GFM, you can *also* use a plugin:
+["remark-gfm"](https://github.com/remarkjs/react-markdown#use).
+It adds support for GitHub-specific extensions to the language:
+tables, strikethrough, tasklists, and literal URLs.
+
+These features **do not work by default**.
+👆 Use the toggle above to add the plugin.
+
+| Feature    | Support  |
+| ---------: | :------------------- |
+| CommonMark | 100%     |
+| GFM        | 100% w/ "remark-gfm" |
+
+~~strikethrough~~
+
+* [ ] task list
+* [x] checked item
+
+https://example.com
+
+## HTML in markdown
+
+⚠️ HTML in markdown is quite unsafe, but if you want to support it, you can
+use ["rehype-raw"](https://github.com/rehypejs/rehype-raw).
+You should probably combine it with
+["rehype-sanitize"](https://github.com/rehypejs/rehype-sanitize).
+
+<blockquote>
+  👆 Use the toggle above to add the plugin.
+</blockquote>
+
+## Components
+
+You can pass components to change things:
+
+
+## More info?
+
+Much more info is available in the
+[readme on GitHub](https://github.com/remarkjs/react-markdown)!
+
+***
+
+A component by [Espen Hovlandsdal](https://espen.codes/)
+
+| Feature    | Support  |
+| ---------: | :------------------- |
+| CommonMark | 100%     |
+| GFM        | 100% w/ "remark-gfm" |`
         },
         {
             type: 'component',
@@ -27,7 +98,7 @@ export async function getChapter(request: ChapterRequest, response: Response) {
         }
     ]
 
-    const navigation: {section: string, id: string, type: 'h1' | 'h2'}[] = [
+    const navigation: { section: string, id: string, type: 'h1' | 'h2' }[] = [
         {
             section: `Chapter ${chapter} in the ${book}`,
             id: `chapter-${chapter}-in-the-${book}`,
